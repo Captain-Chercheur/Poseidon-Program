b4e327638f5c97e3ba27ec70f687461d3369e759
Valentin BRIEZ-BANULS <valentin.briez-banuls@imerir.com>
2021-04-26 12:07:31 +0200

    Database

--- Software/src/main/java/shelf.java
+++ Software/src/main/java/shelf.java
@@ -1,7 +1,19 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class shelf {
-    public static void shelf(String args){
-
-
-
+    public shelf(Stage primaryStage, String storage) {
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(primaryStage);
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.getChildren().add(new Text("This is a Dialog"));
+        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+        dialog.setScene(dialogScene);
+        dialog.show();
     }
+
 }
