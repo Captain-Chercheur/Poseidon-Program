66699cf12f21ca4ffd42cf70b73e9d8051caed96
Valentin BRIEZ-BANULS <valentin.briez-banuls@imerir.com>
2021-05-10 15:03:21 +0200

    Database

--- Database/database.py
+++ Database/database.py
@@ -32,7 +32,7 @@ def get_metas():
 
         for id, reference, state, color, brand, model, year, NIC, storage, weight, barcode, designation, descriptionText, waiting, quantities, accessoire,\
             descriptionComplementaire, quantity, ImageDirectory, ImagesUrl in rows:
-            yield {"items":"[", "id": id, "Nom": designation, "Référence": reference, "Etat": state, "Description": descriptionText,
+            yield "items:[", {"id": id, "Nom": designation, "Référence": reference, "Etat": state, "Description": descriptionText,
                    "Coleur": color, "Poid": weight, "Marque": brand, "Model": model, "Année": year, "NIC": NIC, "storage": storage, "barcode": barcode,
                    "waiting": waiting, "quantities": quantities, "accessoires": accessoire, "descriptionComplementaire": descriptionComplementaire,
                    "quantity:": quantity, "ImageDirectory": ImageDirectory, "imagesUrltoremove":ImagesUrl}
